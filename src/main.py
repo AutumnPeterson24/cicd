943382afb8a86b96abc96af3c9f343d3dec8e282
AutumnPeterson24 <petersonautumn24@gmail.com>
2023-04-22 15:30:32 -0700

    Changed is_odd checker to be an is_cube_root checker

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 """
-odd/even number checker
-Author: Wolf Paulus (https://wolfpaulus.com)
+cube-root number checker
+Author: Autumn Peterson
 """
 
 
-def is_odd(num: int) -> bool:
-    """Return True if num is odd, False otherwise."""
-    return num % 2 == 1
+def is_cube_root(num: int) -> bool:
+    """Return True if num is a cube-root, False otherwise."""
+    return (num ** (1/3) % 3) == 0
 
 
-def is_odd_str(num: str) -> str:
-    """Return a string indicating whether num is odd or even."""
+def is_cube_root_str(num: str) -> str:
+    """Return a string indicating whether num is a cube root."""
     if num.isnumeric():
-        return f"{num} is {'odd' if is_odd(int(num)) else 'even'}."
+        return f"{num} is {'a cube-root' if is_cube_root(int(num)) else 'is not a cube root'}."
     else:
         return "Please enter a number."
