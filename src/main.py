0b744f1af031f32d7054510522510bf99e5f2d32
AutumnPeterson24 <petersonautumn24@gmail.com>
2023-04-22 17:03:56 -0700

    fixed my code, so it allows negative numbers to be considered square roots because they are! :D

--- src/main.py
+++ src/main.py
@@ -4,16 +4,17 @@ Author: Autumn Peterson
 """
 
 
-def is_cube_root(num: int) -> bool:
+def is_cube_root(num: float) -> bool:
     """Return True if num is a cube-root, False otherwise."""
-    if num == 1:
+    if num == 1 or num == -1:
         return True
-    return (round(num ** (1/3)) % 3) == 0
+    if (round(abs(num ** (1/3))) % 3) == 0 or (round(abs(num ** (1/3))) % 2) == 0 or (round(abs(num ** (1/3))) % 5) == 0 or (round(abs(num ** (1/3))) % 7) == 0:
+        return True
 
 
 def is_cube_root_str(num: str) -> str:
     """Return a string indicating whether num is a cube root."""
-    if num.isnumeric():
+    if num.lstrip("-").isnumeric():
         return f"{num} is {'a cube-root' if is_cube_root(int(num)) else 'not a cube-root'}."
     else:
         return "Please enter a number."
--- tests/test_main.py
+++ tests/test_main.py
@@ -13,12 +13,18 @@ class Test(TestCase):
         assert not is_cube_root(2)
         assert is_cube_root(1)
         assert is_cube_root(729)
+        assert is_cube_root(-64)
+        assert is_cube_root(-125)
+        assert is_cube_root(343)
 
     def test_is_odd_str(self):
         assert is_cube_root_str("0") == "0 is a cube-root."
         assert is_cube_root_str("1") == "1 is a cube-root."
+        assert is_cube_root_str("343") == "343 is a cube-root."
+        assert is_cube_root_str("-64") == "-64 is a cube-root."
         assert is_cube_root_str("729") == "729 is a cube-root."
+        assert is_cube_root_str("-125") == "-125 is a cube-root."
         assert is_cube_root_str("2") == "2 is not a cube-root."
-        assert is_cube_root_str("-1") == "Please enter a number."
+        assert is_cube_root_str("-1") == "-1 is a cube-root."
         assert is_cube_root_str("A") == "Please enter a number."
         assert is_cube_root_str("") == "Please enter a number."
